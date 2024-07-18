97bd4c61bc203c4ce8a368b61c2ad2aa70c78bc0
Gunjan Saha <gunjansaha@Gunjans-MacBook-Pro.local>
2024-07-18 21:49:09 +0530

    add

---
+++ fitness 56/android/app/src/main/kotlin/com/fitnessapp/fitness/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fitnessapp.fitness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ fitness 56/lib/common/colo_extension.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TColor {
+    static Color get primaryColor1 => const Color(0xff9BFF5E);
+    static Color get primaryColor2 => const Color(0xff20D628);
+
+    static Color get secondaryColor1 => const Color(0xff8AD5FF);
+    static Color get secondaryColor2 => const Color(0xff7472E9);
+
+  static List<Color> get primaryG => [ primaryColor1, primaryColor2];
+  static List<Color> get secondaryG => [ secondaryColor1, secondaryColor2];
+   
+   static Color get black => const Color(0xff1D1617);
+    static Color get grey => const Color(0xff7B6F72);
+    static Color get white => const Color(0xffFFFFFF);
+}
+
+
+
+
+
+
+
